4ed0531e3b47891bf12b2810c41bd1a4f865d364
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-01-21 03:59:45 +0700

    fakereply with split

--- command/fakereply.js
+++ command/fakereply.js
@@ -4,10 +4,11 @@ module.exports = {
     name: 'fakereply',
     aliases: ['fr', 'fake', 'fitnah'],
     description: 'Untuk memfitnah atau menjahili teman\nPenggunaan !fakereply _tag_',
-    execute (client, chat, pesan, args) {
+    execute (client, chat, pesan) {
         if (!client.isGroup) return client.reply(pesan.error.group)
-        const targets = args[0]
-        const bot = args[1]
+        const arg = client.body.slice(9)
+        const targets = arg.split('|')[1]
+        const bot = arg.split('|')[2]
         mentioned = chat.message.extendedTextMessage.contextInfo.mentionedJid
         client.sendMessage(client.from, `${bot}`, MessageType.text, { quoted: { key: { fromMe: false, participant: `${mentioned}`, ...(client.from ? { remoteJid: client.from } : {}) }, message: { conversation: `${targets}` } } })
     }
