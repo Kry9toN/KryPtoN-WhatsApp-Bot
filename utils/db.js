b84e10e46f3bd912cd8aabe021168abbe24d7365
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-01-20 23:20:11 +0700

    initial for database connections

---
+++ utils/db.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg')
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    connectionTimeoutMillis: 2500,
+    idleTimeoutMillis: 2000,
+    max: 10000
+})
+
+const databaseInput = (value) => new Promise((resolve, reject) => {
+    pool.query(value, (err, result) => {
+        if (err) {
+            console.error(err)
+            reject(err)
+        }
+        resolve(result)
+    })
+})
+
+const databaseView = (value) => new Promise((resolve, reject) => {
+    pool.query(value, (err, result) => {
+        if (err) {
+            console.error(err)
+            reject(err)
+        }
+        resolve(result.rows)
+    })
+})
+
+module.exports = {
+    databaseView,
+    databaseInput
+}
