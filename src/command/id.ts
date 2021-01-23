b435748b8f3afb8069f86b1816ceb3e4dc99a83b
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-01-23 21:17:26 +0700

    add id

---
+++ src/command/id.ts
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'id',
+    cooldown: 10,
+    description: 'Untuk menampilkan id group/user\nPenggunaan: !id',
+    execute (client, chat, pesan) {
+        const uid = client.sender
+        if (client.isGroup) {
+           const gid = client.groupId
+           client.reply(`*ID* kamu : ${uid}\nGroup *ID* : ${gid}`)
+        } else {
+           client.reply(`*ID* kamu : ${uid}`)
+        }
+    }
+}
