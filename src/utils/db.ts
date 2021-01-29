432b049628536631dcdd88c4e5b1de6bcc7cb67e
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-01-29 10:33:54 +0700

    add 2 device conditions

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,11 +1,20 @@
 export {}
+require('dotenv').config()
 const { Pool } = require('pg')
-const pool = new Pool({
+const optionsAndoid = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PW,
+    port: 5432
+}
+const options = {
     connectionString: process.env.DATABASE_URL,
     connectionTimeoutMillis: 2500,
     idleTimeoutMillis: 2000,
     max: 10000
-})
+}
+const pool = new Pool(process.platform == 'android' ? optionsAndoid : options)
 
 const databaseInput = (value: string) => new Promise((resolve, reject) => {
     pool.query(value, (err: string, result: any) => {
