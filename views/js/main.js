72ca3b791ca3530017b8bce17615ac101aa06c68
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-01-30 13:53:08 +0700

    clean up

--- views/js/main.js
+++ views/js/main.js
@@ -1,79 +1,78 @@
 // SOCKET IO
-const socket = io();
+const socket = io()
 // SELECT ELEMENTS
-const labelRam = document.querySelector('.ram-label');
-const labelCpu = document.querySelector('.cpu-label');
-const user = document.querySelector('.user');
-const os = document.querySelector('.os');
-const chatTotal = document.querySelector('.chat');
-const onTime = document.querySelector('.uptime');
-const log = document.querySelector('.log');
-const qrCode = document.querySelector('.qr');
+const labelRam = document.querySelector('.ram-label')
+const labelCpu = document.querySelector('.cpu-label')
+const user = document.querySelector('.user')
+const os = document.querySelector('.os')
+const chatTotal = document.querySelector('.chat')
+const onTime = document.querySelector('.uptime')
+const log = document.querySelector('.log')
+const qrCode = document.querySelector('.qr')
+const notif = new Notification()
 
 // ON CONNECT EVENT
 socket.on('connect', () => {
-    console.log('Connected');
-});
+    console.log('Connected')
+})
 // ON RAM USAGE EVENT
 socket.on('ram-usage', ({ ram, cpu, username, osInfo, chat, uptime, loging }) => {
     // SHOW OS USER INFO
-    user.innerHTML = `<span>Hello ${username}</span>`;
+    user.innerHTML = `<span>Hello ${username}</span>`
     os.innerHTML = `<span>OS type: ${osInfo === 'Windows_NT' ? 'Microsoft Windows' : osInfo}</span>`
     // Set ram label
-    labelRam.innerHTML = `<span>RAM ${ram} % </span>`;
+    labelRam.innerHTML = `<span>RAM ${ram} % </span>`
     // Set Ram bar
-    $('.innerBar-ram').animate({ width: `${ram}%` }, 500);
+    $('.innerBar-ram').animate({ width: `${ram}%` }, 500)
     // Set cpu label
-    labelCpu.innerHTML = `<span>CPU ${cpu} % </span>`;
+    labelCpu.innerHTML = `<span>CPU ${cpu} % </span>`
     // Set cpu bar
-    $('.innerBar-cpu').animate({ width: `${cpu}%` }, 500);
+    $('.innerBar-cpu').animate({ width: `${cpu}%` }, 500)
     // Check
     if (cpu > 90) {
         notify(cpu)
     }
     chatTotal.innerHTML = `<span>Total CHAT: ${chat}</span>`
 
-function botUpTime(seconds){
-  function pad(s){
-    return (s < 10 ? '0' : '') + s;
-  }
-  var hours = Math.floor(seconds / (60*60));
-  var minutes = Math.floor(seconds % (60*60) / 60);
-  var seconds = Math.floor(seconds % 60);
+    function botUpTime (seconds) {
+        function pad (s) {
+            return (s < 10 ? '0' : '') + s
+        }
+        const hours = Math.floor(seconds / (60 * 60))
+        const minutes = Math.floor(seconds % (60 * 60) / 60)
+        seconds = Math.floor(seconds % 60)
 
-  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
-  return onTime.innerHTML = `Uptime: <span>${pad(hours)}Jam ${pad(minutes)}Menit ${pad(seconds)}Detik</span>`
-}
-botUpTime(uptime)
-
-});
+        // return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+        onTime.innerHTML = `Uptime: <span>${pad(hours)}Jam ${pad(minutes)}Menit ${pad(seconds)}Detik</span>`
+    }
+    botUpTime(uptime)
+})
 
 socket.on('log', ({ loging }) => {
-  log.innerHTML = `<span>${loging}</span>`;
+    log.innerHTML = `<span>${loging}</span>`
 })
 
 socket.on('qr-regen', ({ qr }) => {
-  qrCode.innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?data=${qr}&amp;size=250x250" />`;
+    qrCode.innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?data=${qr}&amp;size=250x250" />`
 })
 
 // NOTIFICATION FUNCTION
-let notify = (info) => {
+const notify = (info) => {
     // If granted
     if (Notification.permission === 'granted') {
-        new Notification('Title', {
+        notif('Title', {
             body: `CPU over ${info} %`
-        });
+        })
     }
     // If denied
     if (Notification.permission !== 'denied') {
         Notification.requestPermission()
             .then((permission) => {
                 if (permission === 'granted') {
-                    new Notification('Title', {
+                    notif('Title', {
                         body: `CPU over ${info} %`
-                    });
+                    })
                 };
-            });
+            })
     };
-
-};
+}
