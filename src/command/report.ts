f9a291e2677ea83d16bf7dd60bb52d066c6ea9ba
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-01-30 16:03:20 +0700

    add report

---
+++ src/command/report.ts
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'report',
+    cooldown: 10,
+    description: 'Untuk mereport user\nPenggunaan: _reply_ !report <alasan>',
+    execute (client: any, chat: any, pesan: any, args: any) {
+        if (!client.isGroup) return client.reply(pesan.error.group)
+        const memberList = client.groupAdmins
+        memberList.push(client.sender)
+        if (args > 0) {
+            const options = {
+                text: `Report @${client.sender.split('@')[0]} terkirim ke admin\nAlasan: ${client.body.slice(8)}`,
+                contextInfo: { mentionedJid: memberList },
+                quoted: chat
+            }
+            client.sendMess(client.from, options)
+        } else {
+            const options = {
+                text: `Report @${client.sender.split('@')[0]} terkirim ke admin\nAlasan: tidak ada alasan`,
+                contextInfo: { mentionedJid: memberList },
+                quoted: chat
+            }
+            client.sendMess(client.from, options)
+        }
+    }
+}
