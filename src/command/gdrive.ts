b15f46ab2d330af1906e3559fce15a760f37cfc7
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-01-31 19:20:43 +0700

    fix gdrive command on optimize

--- src/command/gdrive.ts
+++ src/command/gdrive.ts
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 const { google } = require('googleapis')
+const { term } = require('../utils/functions')
+const mime = require('mime-types')
 
 module.exports = {
     name: 'gdrive',
@@ -29,75 +31,78 @@ module.exports = {
 
             // Check if we have previously stored a token.
             fs.readFile(TOKEN_PATH, (err: boolean, token: string) => {
-                if (err) return getAccessToken(oAuth2Client, callback)
+                if (err) return client.reply('Silakan regenerate token dengan cara *!gdrive auth*')
                 oAuth2Client.setCredentials(JSON.parse(token))
                 callback(oAuth2Client)
             })
         }
 
-        /**
-         * Get and store new token after prompting for user authorization, and then
-         * execute the given callback with the authorized OAuth2 client.
-         * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
-         * @param {getEventsCallback} callback The callback for the authorized client.
-         */
-        function getAccessToken (oAuth2Client: any, callback: any) {
-            const authUrl = oAuth2Client.generateAuthUrl({
-                access_type: 'offline',
-                scope: SCOPES
-            })
-            client.reply(`Bukan url ini untuk mendapat kan code auth:  ${authUrl}\n\nKetik: !gdrive auth <code> untuk confirmasi`)
-            const code = args[0] == 'auth' ? args[1] : ''
-            oAuth2Client.getToken(code, (err: boolean, token: string) => {
-                if (err) return console.error('Error retrieving access token', err)
-                oAuth2Client.setCredentials(token)
-                // Store the token to disk for later program executions
-                fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err: string) => {
-                    if (err) return console.error(err)
-                    console.log('Token stored to', TOKEN_PATH)
-                })
-                callback(oAuth2Client)
-            })
-        }
-
-        if (args < 1) {
+        if (args.length <= 1 && client.isUrl(args[0])) {
             /**
              * Describe with given media and metaData and upload it using google.drive.create method()
              */
-            function uploadFile (auth: string) {
-                const drive = google.drive({ version: 'v3', auth })
-                const fileMetadata = {
-                    name: 'photo.jpg'
+            async function uploadFile (auth: string) {
+                const url = args[0]
+                client.reply('Mulai Download file')
+                await term(`aria2c '${url}' --dir=$(pwd)/downloads`).then(() => {
+                    client.reply('Download selesai')
+                    client.reply('Mulai mengupload ke Google Drive\nMungkin membutuhkan waktu yang lama, tunggu aja')
+                }).catch((err: string) => {
+                    client.log(err)
+                    client.reply('Download file gagal')
+                    console.log(err)
+                })
+
+                await fs.readdir(path.join(__dirname, '../../downloads/'), async (err: boolean, nameFile: Array<any>) => {
+                    if (err) return client.reply('File tidak di temukan di folder download')
+                    // 'files' is an array of the files found in the directory
+
+                    const name = nameFile[1]
+                    const drive = google.drive({ version: 'v3', auth })
+                interface meta {
+                    name: string;
+                    [parent: string]: any;
                 }
+                const fileMetadata: meta = {
+                    name: name
+                }
+                if (process.env.GD_ID_DIR !== 'undefined') {
+                    fileMetadata.parent = [process.env.GD_ID_DIR]
+                }
+                const type = mime.lookup(path.join(__dirname, `../../downloads/${nameFile[1]}`))
                 const media = {
-                    mimeType: 'image/jpeg',
-                    body: fs.createReadStream('files/photo.jpg')
+                    mimeType: type,
+                    body: fs.createReadStream(path.join(__dirname, `../../downloads/${nameFile[1]}`))
                 }
-                drive.files.create({
+                await drive.files.create({
                     resource: fileMetadata,
                     media: media,
-                    fields: 'nextPageToken, files(id, name)'
+                    fields: 'id, name'
                 }, (err: string, file: any) => {
                     if (err) {
                         // Handle error
                         console.error(err)
-                        client.log(err)
+                        client.log(`${err}`)
+                        client.reply('Gagal saat mengupload file ke Google Drive')
                     } else {
-                        client.reply(`${file.name} ${BASE_GD.replace(/{}/g, file.id)}`)
+                        client.reply(`Berhasil mengupload file\n🗒️ ${file.data.name}\nLink: ${BASE_GD.replace(/{}/g, file.data.id)}`)
+                        term('rm -rf downloads/*')
                     }
+                })
                 })
             }
 
             // Load client secrets from a local file.
             fs.readFile(path.join(__dirname, '../../credentials.json'), (err: boolean, content: string) => {
-                if (err) return console.log('Error loading client secret file:', err)
+                if (err) return client.log(`Error loading client secret file: ${err}`)
                 // Authorize a client with credentials, then call the Google Drive API.
                 authorize(JSON.parse(content), uploadFile)
             })
         } else if (args[0] == 'auth') {
-            if (args < 1) {
-                fs.readFile(path.join(__dirname, '../../credentials.json'), (err: boolean, credentials: any) => {
+            if (args.length == 1) {
+                fs.readFile(path.join(__dirname, '../../credentials.json'), (err: boolean, content: any) => {
                     if (err) return client.reply(`Error loading client secret file: ${err}`)
+                    const credentials = JSON.parse(content)
                     // eslint-disable-next-line camelcase
                     const { client_secret, client_id, redirect_uris } = credentials.installed
                     const oAuth2Client = new google.auth.OAuth2(
@@ -109,13 +114,14 @@ module.exports = {
                                 access_type: 'offline',
                                 scope: SCOPES
                             })
-                            client.reply(`Bukan url ini untuk mendapat kan code auth:  ${authUrl}\n\nKetik: !gdrive auth <code> untuk confirmasi`)
+                            client.reply(`Bukan url ini untuk mendapat kan code auth:  ${authUrl}\n\nKetik: !gdrive auth token <token> untuk confirmasi`)
                         }
                     })
                 })
-            } else {
-                fs.readFile(path.join(__dirname, '../../credentials.json'), (err: boolean, credentials: any) => {
+            } else if (args.length > 1 && args[1] === 'token') {
+                fs.readFile(path.join(__dirname, '../../credentials.json'), (err: boolean, content: any) => {
                     if (err) return client.reply(`Error loading client secret file: ${err}`)
+                    const credentials = JSON.parse(content)
                     // eslint-disable-next-line camelcase
                     const { client_secret, client_id, redirect_uris } = credentials.installed
                     const oAuth2Client = new google.auth.OAuth2(
@@ -137,15 +143,21 @@ module.exports = {
                 const drive = google.drive({ version: 'v3', auth })
                 drive.files.list({
                     pageSize: 10,
-                    fields: 'nextPageToken, files(id, name)'
+                    fields: 'nextPageToken, files(id, name, mimeType, webViewLink, webContentLink)'
                 }, (err: string, res: any) => {
-                    if (err) return console.log('The API returned an error: ' + err)
+                    if (err) return client.reply(`The API returned an error: ${err}`)
                     const files = res.data.files
                     if (files.length) {
-                        let text = 'Gdrive list files:'
+                        let text = 'Gdrive list files:\n'
                         // eslint-disable-next-line array-callback-return
                         files.map((file: any) => {
-                            text += `${file.name} ${BASE_GD.replace(/{}/g, file.id)}`
+                            if (file.mimeType == 'application/vnd.google-apps.folder') {
+                                const link = file.webViewLink
+                                text += `📂 *${file.name}*\nLink: ${link}\n\n`
+                            } else {
+                                const link = file.webContentLink
+                                text += `🗒️ *${file.name}*\nLink: ${link}\n\n`
+                            }
                         })
                         client.reply(text)
                     } else {
@@ -156,9 +168,51 @@ module.exports = {
 
             // Load client secrets from a local file.
             fs.readFile(path.join(__dirname, '../../credentials.json'), (err: boolean, content: string) => {
-                if (err) return console.log('Error loading client secret file:', err)
+                if (err) return client.log(`Error loading client secret file: ${err}`)
                 // Authorize a client with credentials, then call the Google Drive API.
                 authorize(JSON.parse(content), listFiles)
+            })
+        } else if (args.length > 1 && args[0] == 'mkdir') {
+            /**
+             * Describe with given media and metaData and upload it using google.drive.create method()
+             */
+            function createFolder (auth: string) {
+                client.reply(pesan.tunggu)
+                const name = args[1]
+                const drive = google.drive({ version: 'v3', auth })
+                interface meta {
+                    name: any;
+                    mimeType: string;
+                    [parent: string]: any;
+                }
+                const fileMetadata: meta = {
+                    name: name,
+                    mimeType: 'application/vnd.google-apps.folder'
+
+                }
+                if (process.env.GD_ID_DIR !== 'undefined') {
+                    fileMetadata.parent = [process.env.GD_ID_DIR]
+                }
+                drive.files.create({
+                    resource: fileMetadata,
+                    fields: 'webViewLink, name'
+                }, (err: string, file: any) => {
+                    if (err) {
+                        // Handle error
+                        console.error(err)
+                        client.log(`${err}`)
+                        client.reply('Gagal saat mengupload file ke Google Drive')
+                    } else {
+                        client.reply(`Berhasil memebuat folder\n📂️ ${file.data.name} ${file.data.webViewLink}`)
+                    }
+                })
+            }
+
+            // Load client secrets from a local file.
+            fs.readFile(path.join(__dirname, '../../credentials.json'), (err: boolean, content: string) => {
+                if (err) return client.log(`Error loading client secret file: ${err}`)
+                // Authorize a client with credentials, then call the Google Drive API.
+                authorize(JSON.parse(content), createFolder)
             })
         }
     }
