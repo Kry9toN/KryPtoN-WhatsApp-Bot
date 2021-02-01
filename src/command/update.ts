465d9063c40975b94fc6c08dbd4256ef74f9622a
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-02-01 21:53:52 +0700

    update: fix sendmessage late

--- src/command/update.ts
+++ src/command/update.ts
@@ -2,11 +2,16 @@ export {}
 // using spawn in the child process module
 const spawn = require('child_process').spawn
 const { term, restart } = require('../utils/functions')
+const {
+    MessageType
+} = require('@adiwajshing/baileys')
 
 module.exports = {
     name: 'update',
     description: 'OTA UPDATE Untuk mengupdate bot _only owner_',
     async execute (client: any, chat: any, pesan: any, args: any) {
+        const id = client.from
+        const quoted = chat
         const remote = 'https://' + process.env.GIT_PW + '@github.com/Kry9toN/KryPtoN-WhatsApp-Bot'
         const herokuRemote = 'https://api:' + process.env.HEROKU_API + '@git.heroku.com/krypton-wa.git'
         const genLog = () => new Promise((resolve, reject) => {
@@ -37,52 +42,52 @@ module.exports = {
             })
         })
         if (args.length == 0) {
-            client.reply('Checking OTA update....')
+            client.sendMessage(id, 'Checking OTA update....', MessageType.text, { quoted: quoted })
             term(`git remote add upstream ${remote}`)
                 .then(() => {
                     term('git fetch upstream').then(() => {
                         genLog().then((data: any) => {
                             if (data.length < 30) {
-                                client.reply('Bot dalam kondisi terbaru')
+                                client.sendMessage(id, 'Bot dalam kondisi terbaru', MessageType.text, { quoted: quoted })
                             } else {
-                                client.reply(`OTA UPDATE\n\n${data}\n\nKetik *!update now/deploy* untuk mengupdatenya`)
+                                client.sendMessage(id, `OTA UPDATE\n\n${data}\n\nKetik *!update now/deploy* untuk mengupdatenya`, MessageType.text, { quoted: quoted })
                             }
                         }).catch((err) => console.error(err))
                     }).catch((err: string) => console.error(err))
                 }).catch((err: string) => console.error(err))
         } else if (args.length > 0 && args[0] == 'now') {
-            if (!client.isOwner && !client.isSudo) return client.reply(pesan.hanya.owner)
-            client.reply('Tunggu... bot sedang updating')
+            if (!client.isOwner && !client.isSudo) return client.sendMessage(id, pesan.hanya.owner, MessageType.text, { quoted: quoted })
+            client.sendMessage(id, 'Tunggu... bot sedang updating', MessageType.text, { quoted: quoted })
             term('git reset --hard FETCH_HEAD').then(() => {
-                client.reply('OTA Update berhasil\n Restarting bot....')
+                client.sendMessage(id, 'OTA Update berhasil\n Restarting bot....', MessageType.text, { quoted: quoted })
                 restart()
             }).catch((err: string) => {
                 console.log(err)
                 client.log(err)
-                client.reply('OTA Update gagal/error')
+                client.sendMessage(id, 'OTA Update gagal/error', MessageType.text, { quoted: quoted })
             })
         } else if (args.length > 0 && args[0] == 'deploy') {
-            if (!client.isOwner && !client.isSudo) return client.reply(pesan.hanya.owner)
-            client.reply('Tunggu... bot sedang updating')
+            if (!client.isOwner && !client.isSudo) return client.sendMessage(id, pesan.hanya.owner, MessageType.text, { quoted: quoted })
+            client.sendMessage(id, 'Tunggu... bot sedang updating', MessageType.text, { quoted: quoted })
             term('git reset --hard FETCH_HEAD').then(() => {
                 term(`git remote add heroku ${herokuRemote}`).then(() => {
                     term('git push heroku HEAD:refs/heads/master -f').then(() => {
-                        client.reply('OTA Update berhasil\n Restarting bot....')
+                        client.sendMessage(id, 'OTA Update berhasil\n Restarting bot....', MessageType.text, { quoted: quoted })
                         restart()
                     }).catch((err: string) => {
                         console.log(err)
                         client.log(err)
-                        client.reply('OTA Update gagal/error saat menambah remote')
+                        client.sendMessage(id, 'OTA Update gagal/error saat menambah remote', MessageType.text, { quoted: quoted })
                     })
                 }).catch((err: string) => {
                     console.log(err)
                     client.log(err)
-                    client.reply('OTA Update gagal/error saat deploying')
+                    client.sendMessage(id, 'OTA Update gagal/error saat deploying', MessageType.text, { quoted: quoted })
                 })
             }).catch((err: string) => {
                 console.log(err)
                 client.log(err)
-                client.reply('OTA Update gagal/error')
+                client.sendMessage(id, 'OTA Update gagal/error', MessageType.text, { quoted: quoted })
             })
         }
     }
