ffdfabe5bbf8ff8bac2f235227fc003fd602a030
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-03-03 15:44:30 +0700

    add kopi kanan

--- src/command/promote.ts
+++ src/command/promote.ts
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the KryPtoN Bot WA distribution (https://github.com/Kry9toN/KryPtoN-WhatsApp-Bot).
+ * Copyright (c) 2021 Dhimas Bagus Prayoga.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 module.exports = {
     name: 'promote',
     aliases: ['pm'],
